e4a4d8d7abb836424f232409afdde1ccc19dd20f
Brandon Unzueta <unzueta.brandon@gmail.com>
2020-05-11 19:33:48 -0700

    setting up css files for tailwind

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img className="w-full" src="https://source.unsplash.com/random" alt="" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-l mb-2 text-center">
+          Photo by Joe Shmoe.
+        </div>
+      </div>
     </div>
   );
 }

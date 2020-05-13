1b7504b92c635d2f005350e89bd0be8f57ace664
Brandon Unzueta <unzueta.brandon@gmail.com>
2020-05-12 20:23:01 -0700

    creating component for image cards

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ImageCard from "./components/ImageCard";
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img className="w-full" src="https://source.unsplash.com/random" alt="" />
-      <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-l mb-2 text-center">
-          Photo by Joe Shmoe.
-        </div>
+    <div className="container mx-auto">
+      <div className="grid grid-cols-3 gap-4">
+        {images.map((image) => (
+          <ImageCard key={image.id} image={image} />
+        ))}
       </div>
     </div>
   );
